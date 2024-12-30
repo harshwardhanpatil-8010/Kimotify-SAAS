6ceb6e5fee40e15e1890fd84e7edb9adc6672c7f
Harshwardhan Patil <163681828+harshwardhanpatil-8010@users.noreply.github.com>
2024-12-30 19:34:13 +0530

    Update dashboard.ts

--- src/constants/dashboard.ts
+++ src/constants/dashboard.ts
@@ -1,38 +1,45 @@
 import { v4 } from 'uuid'
+import { onBoardUser } from '@/actions/user'
+import { redirect } from 'next/navigation'
 
 type Props = {
   id: string
   label: string
   subLabel: string
   description: string
-  link: string 
+  link: string
 }
-const user = {
-  data: {
-    firstname: 'John',
-    lastname: 'Doe'
+
+export const getDashboardCards = async (): Promise<Props[]> => {
+  const user = await onBoardUser();
+
+  if (!user || (user.status !== 200 && user.status !== 201)) {
+    redirect('/sign-in');
   }
-}
-export const DASHBOARD_CARDS: Props[] = [
-  {
-    id: v4(),
-    label: 'Set-up Auto Replies',
-    subLabel: 'Deliver a product lineup through Instagram DM',
-    description: 'Get products in front of your followers in as many places',
-    link: '/Help' // Add a link to navigate to
-  },
-  {
-    id: v4(),
-    label: 'Answer Questions with AI',
-    subLabel: 'Identify and respond to queries with AI',
-    description: 'The intention of the message will be automatically detected',
-    link: `dashboard/${user.data?.firstname}${user.data?.lastname}/automations` // Add a link to navigate to
-  },
-  {
-    id: v4(),
-    label: 'Schedule Posts Automatically',
-    subLabel: 'Plan your posts ahead of time and automate publishing',
-    description: 'Save time by automating your social media posts',
-    link: '/Help/schedule-posts' // Add a link to navigate to
-  },
-]
+
+  const userFullName = `${user.data?.firstname}${user.data?.lastname}`;
+
+  return [
+    {
+      id: v4(),
+      label: 'Set-up Auto Replies',
+      subLabel: 'Deliver a product lineup through Instagram DM',
+      description: 'Get products in front of your followers in as many places',
+      link: '/Help', 
+    },
+    {
+      id: v4(),
+      label: 'Answer Questions with AI',
+      subLabel: 'Identify and respond to queries with AI',
+      description: 'The intention of the message will be automatically detected',
+      link: `/dashboard/${user.data?.firstname}${user.data?.lastname}/automations`,
+    },
+    {
+      id: v4(),
+      label: 'Schedule Posts Automatically',
+      subLabel: 'Plan your posts ahead of time and automate publishing',
+      description: 'Save time by automating your social media posts',
+      link: '/Help/schedule-posts', // Static link
+    },
+  ];
+};
